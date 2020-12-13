6c8775a4fa4eb024e7e09565fe7db7777d654747
Ivelina <ivelinavul@gmail.com>
2020-12-13 16:01:51 +0200

    Seperate content into different sections

--- javascript/default.js
+++ javascript/default.js
@@ -16,20 +16,3 @@ function deactivateButton(buttonQuery){
 function activateButton(buttonQuery){
   document.querySelector(buttonQuery).className+=" active";
 }
-function copyToClipboard(){
-  var text= event.target;//.textContent;
- console.log(event);
-  text.select();
-  document.execCommand("copy");
-  alert("Копиран текст");
-
-// /* Select the text field */
-// copyText.select();
-// copyText.setSelectionRange(0, 99999); /*For mobile devices*/
-//
-// /* Copy the text inside the text field */
-// document.execCommand("copy");
-//
-// /* Alert the copied text */
-// alert("Copied the text: " + copyText.value);
-}
